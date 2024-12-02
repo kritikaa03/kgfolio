764b559c706e70af1182018ce18452896bb7d578
Kritika Giri <kritikagiri03@gmail.com>
2024-12-02 20:24:19 +0530

    fixed imports

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -4,8 +4,6 @@ import myImg from "../../Assets/girl.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
